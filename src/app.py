487b9eb2a1706872cbbdba4bf701307de46a0ac1
marjorieshigaki <158107113+marjorieshigaki@users.noreply.github.com>
2025-03-24 15:46:39 +0000

    Melhora a validação de inscrição para impedir que alunos se inscrevam novamente na mesma atividade

--- src/app.py
+++ src/app.py
@@ -98,9 +98,9 @@ def signup_for_activity(activity_name: str, email: str):
     # Get the specificy activity
     activity = activities[activity_name]
 
-    # Validar se o aluno já está inscrito
+   # Validate student is not already signed up
     if email in activity["participants"]:
-        return {"message": f"{email} is already signed up for {activity_name}"}
+        raise HTTPException(status_code=400, detail="Student already signed up for this activity")
     
     # Add student
     activity["participants"].append(email)
